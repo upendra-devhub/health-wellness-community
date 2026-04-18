0e2c1d9cbbec6422b8612c0a6609a8eec86bf757
aryan2110 <kunalgupta2110@gmail.com>
2026-04-18 18:50:16 +0530

    Revert "feat: add interactive health dashboard with activity tracking"
    This reverts commit e2ce5a245b727b6d671c07a69186390447ec2695.

--- src/models/HealthTracker.js
+++ src/models/HealthTracker.js
@@ -1,43 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const dailyLogSchema = new Schema({
-  date: {
-    type: String,
-    required: true,
-    match: /^\d{4}-\d{2}-\d{2}$/
-  },
-  walkingSteps: {
-    type: Number,
-    min: 0,
-    default: 0
-  },
-  runningKm: {
-    type: Number,
-    min: 0,
-    default: 0
-  },
-  sleepHours: {
-    type: Number,
-    min: 0,
-    default: 0
-  },
-  waterIntake: {
-    type: Number,
-    min: 0,
-    default: 0
-  },
-  notes: {
-    type: String,
-    trim: true,
-    maxlength: 280,
-    default: ''
-  }
-}, {
-  _id: false,
-  timestamps: true
-});
-
 const healthTrackerSchema = new Schema({
   userId: {
     type: Schema.Types.ObjectId,
@@ -56,15 +19,7 @@ const healthTrackerSchema = new Schema({
   steps: {
     type: Number,
     default: 0
-  },
-  dailyLogs: {
-    type: [dailyLogSchema],
-    default: []
   }
-}, {
-  timestamps: true
 });
-
-healthTrackerSchema.index({ userId: 1 });
 
 module.exports = mongoose.model('HealthTracker', healthTrackerSchema);
